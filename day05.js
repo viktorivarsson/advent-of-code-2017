91c31a9247f5cbcfdf47171752d95acc44fd375f
Viktor Ivarsson <viktor.ivarsson@deltaprojects.com>
2017-12-05 23:49:41 +0100

    Add solution for day 5

---
+++ day05.js
@@ -0,0 +1,58 @@
+// Read input
+const input = require('fs').readFileSync('day05.txt', 'utf8');
+
+// Return if a string is not empty
+const notEmpty = x => x !== '';
+
+// Get input as an array of lines, removing empty lines and casting to numbers
+const lines = input
+	.split('\n')
+	.filter(notEmpty)
+	.map(Number);
+
+const testLines = [0, 3, 0, 1, -3];
+
+/**
+ * PART 1
+ */
+
+// Get total number of jumps required
+const getJumpsRequired = (inputMaze, part) => {
+	// Make maze a copy of the input maze to not mutate the original
+	const maze = [...inputMaze];
+
+	let currentIndex = 0;
+	let steps = 0;
+
+	// While the current index is smaller than the maze length
+	while (currentIndex < maze.length) {
+		// Set next index to the sum of current index and the value of current index
+		let nextIndex = currentIndex + maze[currentIndex];
+
+		// If part 1 or value of current index is smaller than three,
+		// increase the value of current index. If not, decrease it
+		if (part === 1 || maze[currentIndex] < 3) {
+			maze[currentIndex]++;
+		} else {
+			maze[currentIndex]--;
+		}
+
+		// Set current index to the next calculated index
+		currentIndex = nextIndex;
+
+		// Increase the steps taken
+		steps++;
+	}
+
+	return steps;
+};
+
+const part1 = getJumpsRequired(lines, 1);
+console.log(part1);
+
+/**
+ * PART 2
+ */
+
+const part2 = getJumpsRequired(lines, 2);
+console.log(part2);
